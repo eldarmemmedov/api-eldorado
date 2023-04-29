26e94f0df47bdd16d7730164f63a1ce49bd83d27
emammadov3 <eldarmammadov4@gmail.com>
2023-04-29 18:15:40 +0400

    Initial commit

---
+++ src/main/java/com/eldorado/ApiEldoradoApplication.java
@@ -0,0 +1,13 @@
+package com.eldorado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiEldoradoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiEldoradoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eldorado/ApiEldoradoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eldorado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiEldoradoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
